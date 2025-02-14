338ddd6f0491f14b81f320c47b5f1cfdc83776b4
Sahrul Romadi <sahrulromadi12@gmail.com>
2025-02-14 23:20:09 +0700

    feat: :tada: prepare folders

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="flex flex-col min-h-screen space-y-10 pb-10 bg-background">
+      <p>Home</p>
+    </main>
+  );
+};
+
+export default Home;
